a6657f71faca412acf1e7d76442378bd68d2e8d2
spookiebruh <zackeboibeat@gmail.com>
2022-09-02 15:23:55 +0200

    Working play and stop button using pygame

--- Mp3spelare/zamppygameversion.py
+++ Mp3spelare/zamppygameversion.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import filedialog
 from pygame import mixer
 
             
@@ -10,8 +11,70 @@ window.geometry("350x550")
 
 mixer.init()
 
-song_list = Listbox(window, bg="black", fg="cyan", width=50).pack(pady=20)
+#Function to add song
+def add_song():
+    song = filedialog.askopenfilename(initialdir='C:/Users/zackarias.edlundsve/Music', title="Choose song", filetypes=(("mp3 Files", "*.mp3"), ("wav Files", "*.wav"), ))
 
+    #Remove file path info and .mp3/.wav
+    song = song.replace("C:/Users/zackarias.edlundsve/Music/", "")
+    song = song.replace(".mp3", "")
+    song = song.replace(".wav", "")
+
+    #Insert file into song list
+    song_list.insert(END, song)
+
+#Play song
+def play():
+    song = song_list.get(ACTIVE)
+    song = f'C:/Users/zackarias.edlundsve/Music/{song}.mp3'
+    mixer.music.load(song)
+    mixer.music.play(loops=0)
+
+#Stop current song
+def stop():
+    mixer.music.stop()
+    song_list.selection_clear(ACTIVE)
+
+#Pause current song
+def pause():
+    pausestate = 0
+    if pausestate == 0:
+        mixer.music.pause()
+        pausestate = 1
+    elif pausestate == 1:
+        mixer.music.unpause()
+        pausestate = 0
+
+#Song list
+song_list = Listbox(window, bg="black", fg="cyan", width=50, selectbackground="gray", selectforeground="black")
+song_list.pack(pady=20)
+
+
+#Buttons control frame
+controls_frame = Frame(window)
+controls_frame.pack()
+
+#Create buttons for player
+next_button = Button(controls_frame, text="Next")
+prev_button = Button(controls_frame,text="Prev")
+play_button = Button(controls_frame,text="Play", command=play)
+pause_button = Button(controls_frame,text="Pause", command=pause)
+stop_button = Button(controls_frame,text="Stop", command=stop)
+
+next_button.grid(row=0, column=0)
+prev_button.grid(row=0, column=1)
+play_button.grid(row=0, column=2)
+pause_button.grid(row=0, column=3)
+stop_button.grid(row=0, column=4)
+
+#Create menu
+top_menu = Menu(window)
+window.config(menu=top_menu)
+
+#Song to add songs
+menu_addSong = Menu(top_menu)
+top_menu.add_cascade(label="Add song", menu=menu_addSong)
+menu_addSong.add_command(label="Add one song to playlist", command=add_song)
 
 window.mainloop()
 
